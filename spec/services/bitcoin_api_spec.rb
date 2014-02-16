5bf725a045dd028ef263467804f653313fe4aa8c
Scott Li <scottli0101@gmail.com>
2014-02-16 19:28:20 +1100

    cleaned satoshi specs

--- spec/services/bitcoin_api_spec.rb
+++ spec/services/bitcoin_api_spec.rb
@@ -4,21 +4,21 @@ describe BitcoinAPI do
 	context(:get_unspents) do
 		let(:from_address) { "1MTdy3kHGnuM9ARPWfSyAHPS8jdXf5kJJk" }
 		it "get unspents from a given address" do
-			total_value = 0.001.to_satoshi + FEE
+			total_value = 0.001.to_satoshis + FEE
 			unspents = BitcoinAPI.get_unspents(from_address, total_value)
 			unspents.length.should eq(1)
-			unspents[0]["value"].should eq(0.005.to_satoshi)
+			unspents[0]["value"].should eq(0.005.to_satoshis)
 		end
 
 		it "gets both unspents" do
-			total_value = 0.005.to_satoshi + FEE
+			total_value = 0.005.to_satoshis + FEE
 			unspents = BitcoinAPI.get_unspents(from_address, total_value)
 			unspents.length.should eq(2)
-			unspents.unspent_value.should eq(0.006.to_satoshi)
+			unspents.unspent_value.should eq(0.006.to_satoshis)
 		end
 
 		it "raises error if amount in sufficient" do
-			total_value = 0.006.to_satoshi + FEE
+			total_value = 0.006.to_satoshis + FEE
 			expect {BitcoinAPI.get_unspents(from_address, total_value) }.to raise_error(BitcoinAPI::InsufficientAmount)
 		end
 	end
